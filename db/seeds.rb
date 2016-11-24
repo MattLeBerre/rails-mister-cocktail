b8db2e3c8d7688a161dc3391addd6293b6ea27f4
Matthieu Le Berre <matthieu.leberre@gmail.com>
2016-11-24 17:33:51 +0100

    going on

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+cocktail1= Cocktail.new(name: "Mojito")
+cocktail1.save
+cocktail2= Cocktail.new(name: "Sex on the beach")
+cocktail2.save
+cocktail3= Cocktail.new(name: "Caipiroska")
+cocktail3.save
+
+
